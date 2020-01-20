be03075005682fd392d864bb5a94ff786c567b15
CircleCI <brett@annalytics.co.uk>
2020-01-20 13:05:57 +0000

    Conform to updated API
    Add functionality for error responses
    Doc fixes
    Update package lock
    Update packages

--- lib/payment-api-cordova.spec.ts
+++ lib/payment-api-cordova.spec.ts
@@ -5,15 +5,11 @@ import { CordovaMock } from "./__mocks__/cordova-mock";
 
 window.cordova = new CordovaMock("hello.test.api", true, "hello.processing.ver");
 
-function verfiyObservableValues<T>(observable: Observable<T>, vals: T[], done: jest.DoneCallback) {
+function verfiyPromiseValue<T>(observable: Promise<T>, expected: T, done: jest.DoneCallback) {
     var indx = 0;
-    observable.subscribe({
-        next: value => {
-            expect(value).toBe(vals[indx++]);
-            if (indx == vals.length) {
-                done();
-            }
-        }
+    observable.then(value => {
+        expect(value).toBe(expected);
+        done();
     });
 }
 
@@ -24,19 +20,19 @@ describe('PaymentApiCordova', () => {
 
     it('should return a version number', done => {
         var version = PaymentApiCordova.getInstance().getApiVersion();
-        let vals = ["Unknown", "hello.test.api"];
-        verfiyObservableValues(version, vals, done);
+        let vals = "hello.test.api";
+        verfiyPromiseValue(version, vals, done);
     });
 
     it('should return installed correctly', done => {
         var installed = PaymentApiCordova.getInstance().isProcessingServiceInstalled();
-        let vals = [false, true];
-        verfiyObservableValues(installed, vals, done);
+        let vals = true;
+        verfiyPromiseValue(installed, vals, done);
     });
 
     it('should return processing service version number', done => {
         var version = PaymentApiCordova.getInstance().getProcessingServiceVersion();
-        let vals = ["Unknown", "hello.processing.ver"];
-        verfiyObservableValues(version, vals, done);
+        let vals = "hello.processing.ver";
+        verfiyPromiseValue(version, vals, done);
     });
 });
